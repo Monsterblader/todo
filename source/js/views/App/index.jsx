ac23feef518c2fd852824585781a8ac4b99ddc36
Monsterblader <Monsterblader@gmail.com>
2017-07-27 13:10:21 -0700

    Successfully implements DragSource from react-dnd.

--- source/js/views/App/index.jsx
+++ source/js/views/App/index.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
 
 import { addTask } from 'actions/app';
 
 import CreateTask from 'views/CreateTask';
 import TaskList from 'views/TaskList';
 
+@DragDropContext(HTML5Backend)
 export default class App extends Component {
   static propTypes = {
     children: PropTypes.object,
--- source/js/views/Task/index.jsx
+++ source/js/views/Task/index.jsx
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { DragSource } from 'react-dnd';
+import { ItemTypes } from 'Constants';
 
+const taskSource = {
+  beginDrag(props) {
+    return {id: props.id};
+  },
+
+  endDrag(props, monitor, component) {
+    if (!monitor.didDrop()) {
+      // You can check whether the drop was successful
+      // or if the drag ended but nobody handled the drop
+      return;
+    }
+
+    // When dropped on a compatible target, do something.
+    // Read the original dragged item from getItem():
+    const item = monitor.getItem();
+
+    // You may also read the drop result from the drop target
+    // that handled the drop, if it returned an object from
+    // its drop() method.
+    const dropResult = monitor.getDropResult();
+
+    // This is a good place to call some Flux action
+//    CardActions.moveCardToList(item.id, dropResult.listId);
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
+@DragSource(ItemTypes.TASK, taskSource, collect)
 export default class Task extends Component {
   static propTypes = {
     value: PropTypes.object,
-  }
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+  };
 
   render() {
+    const {connectDragSource, isDragging} = this.props;
     const {value} = this.props;
 
-    return (
-        <div className='singleTask'>
+    return connectDragSource(
+        <div style={{
+                opacity: isDragging ? 0.5 : 1,
+                fontSize: 25,
+                fontWeight: 'bold',
+                cursor: 'move'}}
+        className='singleTask'>
           <p>
             {value.task} {value.startDate} {value.endDate}
           </p>
