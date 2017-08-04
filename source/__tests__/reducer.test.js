b2fd61a03897d13b7e1472fa5f9042bf21aa375b
Monsterblader <Monsterblader@gmail.com>
2017-08-03 21:39:53 -0700

    Implements working test set ups.

--- source/__tests__/reducer.test.js
+++ source/__tests__/reducer.test.js
@@ -4,15 +4,21 @@ import mainReducer from '../js/reducers';
 describe('The main reducer', () => {
   it('should create add a task to the state when ADD_TASK is passed.', () => {
     const initialState = fromJS({taskList: []});
-    expect(mainReducer(undefined, fromJS({
+    expect(mainReducer(undefined, {
       type: 'ADD_TASK',
-      action: {
+      task: Map({
         endDate: "",
-        index: "",
+        index: "0",
         parent: "",
         startDate: "",
-        task: "",
-      }
-    }))).toBe(4);
+        task: "1",
+      })
+    })).toEqual({app: fromJS({taskList: [{
+      endDate: "",
+      index: 0,
+      parent: "",
+      startDate: "",
+      task: "1",
+    }]})});
   });
 });
